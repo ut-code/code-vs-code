ffd68835cfb0c7d219067c99e747766ebca4e971
chelproc <chelproc@outlook.com>
2022-09-06 02:27:11 +0000

    init

---
+++ model.ts
@@ -0,0 +1,39 @@
+// イメージです
+
+interface Location {
+  x: number;
+  y: number;
+}
+
+interface GameObject {
+  location: Location;
+}
+
+interface Fighter extends GameObject {
+  hp: number;
+}
+
+interface Player extends Fighter {
+  /** 3 つまで */
+  weapons: Weapon[];
+
+  walkTo(location: Location);
+  attack(fighter: Fighter);
+  useWeapon(weapon: Weapon);
+}
+
+interface Tool extends GameObject {
+  type: "回復" | "スピードアップ" | "火力アップ";
+  effectSize: number;
+};
+
+interface Weapon {
+  range: number;
+  damage: number;
+};
+
+declare const player: Player;
+declare const enemies: Fighter[];
+declare const tools: Tool[];
+
+declare function getDistance(a: GameObject, b: GameObject): number;
