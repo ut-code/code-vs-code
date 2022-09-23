b4aa2e0695a4874cb0917e7ef648f41be68f206c
chelproc <chelproc@outlook.com>
2022-09-23 06:59:27 +0000

    model.ts の更新

--- model.ts
+++ model.ts
@@ -1,40 +1,59 @@
 /* eslint-disable */
 // イメージです
 
-interface Location {
-  x: number;
-  y: number;
+interface ベクトル2 {
+  X: number;
+  Y: number;
 }
 
-interface GameObject {
-  location: Location;
+interface 存在 {
+  場所: ベクトル2;
 }
 
-interface Fighter extends GameObject {
-  hp: number;
+interface ファイター extends 存在 {
+  HP: number;
+  速度: ベクトル2; // 1 フレームの移動
+  元気: number;
+  所持武器: 武器 | null;
+  残り武器リロードフレーム: number;
 }
 
-interface Player extends Fighter {
-  /** 3 つまで */
-  weapons: Weapon[];
-
-  walkTo(location: Location);
-  attack(fighter: Fighter);
-  useWeapon(weapon: Weapon);
+interface ポーション {
+  種類: "スピードアップ" | "火力アップ";
+  効果: number;
 }
 
-interface Tool extends GameObject {
-  type: "回復" | "スピードアップ" | "火力アップ";
-  effectSize: number;
+interface 武器 extends 存在 {
+  射程: number;
+  弾の大きさ: number;
+  弾速: number;
+  リロードフレーム: number;
+  消費元気: number;
 }
 
-interface Weapon {
-  range: number;
-  damage: number;
+declare const 自分: ファイター;
+declare const 全ての敵: ファイター[];
+declare const 落ちている全てのポーション: ポーション[];
+
+// 定数
+declare const 腕の長さ: number;
+
+// 初心者向け
+declare const いちばん近い敵: ファイター;
+
+declare function aとbの距離(a: ベクトル2 | 存在, b: ベクトル2 | 存在): number;
+
+// 意思決定
+declare function xへ向かう(x: ベクトル2 | 存在); // 元気は減らない
+declare function xへ走る(x: ベクトル2 | 存在); // -3 元気
+declare function xを殴る(x: 存在);  // -1 元気
+declare function xに対して武器を使う(x: ベクトル2 | 存在); // -x.消費元気 元気
+declare function xを拾う(x: 武器);
+
+// 低年齢の子に書いてもらいたい
+for (const 敵 of 全ての敵) {
+  if (aとbの距離(敵, 自分) < 腕の長さ) {
+    xを殴る(敵);
+  }
 }
-
-declare const player: Player;
-declare const enemies: Fighter[];
-declare const tools: Tool[];
-
-declare function getDistance(a: GameObject, b: GameObject): number;
+xへ向かう(いちばん近い敵);
