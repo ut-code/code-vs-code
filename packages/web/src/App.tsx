c28a408d0930936c0d70ee929ac44becaa99e531
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2022-10-19 12:13:01 +0900

    add openAPI

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,36 +1,41 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Blockly from "blockly";
+import Ja from "blockly/msg/ja";
 import "./style.css";
+import options from "./options";
+
+Blockly.setLocale(Ja);
+Blockly.HSV_SATURATION = 0.6;
+Blockly.HSV_VALUE = 1;
 
 function Injection() {
-  const workspaceRef = useRef<HTMLDivElement>(null);
+  const [code, setCode] = useState("");
+  const workspaceDivRef = useRef<HTMLDivElement>(null);
+  const workspaceRef = useRef<Blockly.WorkspaceSvg>();
   useEffect(() => {
-    // https://github.com/google/blockly/issues/6075
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const options: any = {
-      toolbox: {
-        kind: "flyoutToolbox",
-        contents: [
-          {
-            kind: "block",
-            type: "controls_if",
-          },
-          {
-            kind: "block",
-            type: "controls_whileUntil",
-          },
-        ],
-      },
-    };
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
-    const workspace = Blockly.inject(workspaceRef.current, options);
+    const workspace = Blockly.inject(workspaceDivRef.current, options);
+    workspaceRef.current = workspace;
     return () => {
       workspace.dispose();
     };
   }, []);
 
-  return <div ref={workspaceRef} />;
+  return (
+    <>
+      <div ref={workspaceDivRef} />
+      <button
+        type="button"
+        onClick={() => {
+          setCode(Blockly.JavaScript.workspaceToCode(workspaceRef.current));
+        }}
+      >
+        出力
+      </button>
+      {code}
+    </>
+  );
 }
 
 export default function App() {
---
+++ packages/web/src/options.ts
@@ -0,0 +1,260 @@
+import {
+  ATTACK,
+  ENEMIES,
+  GET_PROPERTY_OF_FIGHTER,
+  PICK_UP,
+  PLAYER,
+  PORTIONS,
+  RUN_TO,
+  USE_WEAPON,
+  MATH_VECTOR2,
+  WALK_TO,
+  WEAPONS,
+  GET_PROPERTY_OF_PORTION,
+  GET_PROPERTY_OF_WEAPON,
+} from "./blocks";
+
+const numberInput = (initialInput: number) => {
+  return {
+    shadow: {
+      type: "math_number",
+      fields: {
+        NUM: initialInput,
+      },
+    },
+  };
+};
+
+// https://github.com/google/blockly/issues/6075
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const options: any = {
+  toolbox: {
+    kind: "categoryToolbox",
+    contents: [
+      {
+        kind: "category",
+        name: "論理",
+        contents: [
+          {
+            kind: "block",
+            type: "controls_if",
+          },
+          {
+            kind: "block",
+            type: "logic_compare",
+            inputs: {
+              A: numberInput(0),
+              B: numberInput(0),
+            },
+          },
+          {
+            kind: "block",
+            type: "logic_operation",
+          },
+          {
+            kind: "block",
+            type: "logic_negate",
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "ループ",
+        contents: [
+          {
+            kind: "block",
+            type: "controls_repeat_ext",
+            inputs: {
+              TIMES: numberInput(10),
+            },
+          },
+          {
+            kind: "block",
+            type: "controls_whileUntil",
+          },
+          {
+            kind: "block",
+            type: "controls_for",
+            inputs: {
+              FROM: numberInput(1),
+              TO: numberInput(10),
+              BY: numberInput(1),
+            },
+          },
+          {
+            kind: "block",
+            type: "controls_forEach",
+          },
+          {
+            kind: "block",
+            type: "controls_flow_statements",
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "数",
+        contents: [
+          {
+            kind: "block",
+            type: "math_number",
+          },
+          {
+            kind: "block",
+            type: MATH_VECTOR2,
+            inputs: {
+              X: numberInput(0),
+              Y: numberInput(0),
+            },
+          },
+          {
+            kind: "block",
+            type: "math_arithmetic",
+            inputs: {
+              A: numberInput(0),
+              B: numberInput(0),
+            },
+          },
+          {
+            kind: "block",
+            type: "math_modulo",
+            inputs: {
+              DIVIDEND: numberInput(0),
+              DIVISOR: numberInput(0),
+            },
+          },
+          {
+            kind: "block",
+            type: "math_single",
+            inputs: {
+              NUM: numberInput(0),
+            },
+          },
+          {
+            kind: "block",
+            type: "math_trig",
+            inputs: {
+              NUM: numberInput(0),
+            },
+          },
+          {
+            kind: "block",
+            type: "math_constant",
+          },
+          {
+            kind: "block",
+            type: "math_random_int",
+            inputs: {
+              FROM: numberInput(1),
+              TO: numberInput(10),
+            },
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "情報",
+        contents: [
+          {
+            kind: "block",
+            type: PLAYER,
+          },
+          {
+            kind: "block",
+            type: ENEMIES,
+          },
+          {
+            kind: "block",
+            type: PORTIONS,
+          },
+          {
+            kind: "block",
+            type: WEAPONS,
+          },
+          {
+            kind: "block",
+            type: GET_PROPERTY_OF_FIGHTER,
+          },
+          {
+            kind: "block",
+            type: GET_PROPERTY_OF_PORTION,
+          },
+          {
+            kind: "block",
+            type: GET_PROPERTY_OF_WEAPON,
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "行動",
+        contents: [
+          {
+            kind: "block",
+            type: WALK_TO,
+          },
+          {
+            kind: "block",
+            type: RUN_TO,
+          },
+          {
+            kind: "block",
+            type: ATTACK,
+          },
+          {
+            kind: "block",
+            type: USE_WEAPON,
+          },
+          {
+            kind: "block",
+            type: PICK_UP,
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "変数",
+        custom: "VARIABLE",
+      },
+      {
+        kind: "category",
+        name: "リスト",
+        contents: [
+          {
+            kind: "block",
+            type: "lists_create_with",
+          },
+          {
+            kind: "block",
+            type: "lists_repeat",
+            inputs: {
+              NUM: numberInput(5),
+            },
+          },
+          {
+            kind: "block",
+            type: "lists_getIndex",
+            inputs: {
+              AT: numberInput(1),
+            },
+          },
+          {
+            kind: "block",
+            type: "lists_setIndex",
+            inputs: {
+              AT: numberInput(1),
+              TO: numberInput(0),
+            },
+          },
+        ],
+      },
+      {
+        kind: "category",
+        name: "関数",
+        custom: "PROCEDURE",
+      },
+    ],
+  },
+};
+
+export default options;
