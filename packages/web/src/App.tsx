1a11f69cc9889931d35cfce0d286b1284a848943
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2022-10-19 12:26:39 +0900

    add openAPI

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useRef, useState } from "react";
 import Blockly from "blockly";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import Ja from "blockly/msg/ja";
 import "./style.css";
 import options from "./options";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 Blockly.setLocale(Ja);
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 Blockly.HSV_SATURATION = 0.6;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 Blockly.HSV_VALUE = 1;
 
 function Injection() {
