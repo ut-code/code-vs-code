1f7d4c5cb36e02c2a9ee6a705fdafbc812c88d52
E-nana17 <104970804+E-nana17@users.noreply.github.com>
2022-10-19 12:30:27 +0900

    add openAPI

--- packages/web/src/config/blockly.ts
+++
@@ -1,9 +0,0 @@
-import * as Blockly from "blockly/core";
-import "blockly/blocks";
-import "blockly/javascript";
-import * as Ja from "blockly/msg/ja";
-
-Blockly.setLocale(Ja);
-
-Blockly.HSV_SATURATION = 0.6;
-Blockly.HSV_VALUE = 1;
