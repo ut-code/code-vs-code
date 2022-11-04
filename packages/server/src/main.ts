9f0c840e64cb15b7181e42152409791393cc1976
fuga <104971005+Mtyuya@users.noreply.github.com>
2022-11-04 23:05:05 +0900

    add api

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const client = new PrismaClient();
 
 app.use(cors({ origin: process.env["WEB_ORIGIN"] }));
 
 app.get("/", (_, res) => {
   res.json({ message: "success" });
+});
+
+app.put("/program", async (request, response) => {
+  await client.userBattleIdentity.create({
+    data: { program: request.body.program },
+  });
+  response.send();
 });
 
 app.listen(8081);
