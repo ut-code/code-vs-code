77bdbf048948344a4a09b704c93ef4b429b9d7fd
fuga <104971005+Mtyuya@users.noreply.github.com>
2022-11-05 16:25:57 +0900

    add api

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -7,13 +7,42 @@ const client = new PrismaClient();
 
 app.use(cors({ origin: process.env["WEB_ORIGIN"] }));
 
+// 通信テスト
 app.get("/", (_, res) => {
   res.json({ message: "success" });
 });
 
+// Userテーブルの作成
+app.post("/user", async (request, response) => {
+  await client.user.create({
+    data: { name: request.body.name },
+  });
+  response.send();
+});
+
+// Userを全て取得
+app.get("/user", async (_, response) => {
+  const users = await client.user.findMany();
+  response.json(users);
+});
+
+// 名前の変更
+app.put("/user", async (request, response) => {
+  await client.user.update({
+    where: { id: request.body.id },
+    data: { name: request.body.name },
+  });
+  response.send();
+});
+
+// プログラムの挿入とUserBattleIdentityの作成
 app.put("/program", async (request, response) => {
   await client.userBattleIdentity.create({
-    data: { program: request.body.program },
+    data: {
+      user: client.user,
+      program: request.body.program,
+      league: 1,
+    },
   });
   response.send();
 });
