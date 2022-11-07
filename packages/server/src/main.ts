fccdc2a61b97f52e7cd116bee3553420b2a80a32
fuga <104971005+Mtyuya@users.noreply.github.com>
2022-11-08 00:09:22 +0900

    modify 3 APIs, add 1 API

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -6,42 +6,90 @@ const app = express();
 const client = new PrismaClient();
 
 app.use(cors({ origin: process.env["WEB_ORIGIN"] }));
+app.use(express.json());
 
 // 通信テスト
+
 app.get("/", (_, res) => {
   res.json({ message: "success" });
 });
 
-// Userテーブルの作成
+// Userの作成
+
+type PostUserRequest = {
+  name: string;
+};
+
+type PostUserResponse = {
+  id: number;
+  name: string;
+};
+
 app.post("/user", async (request, response) => {
+  const requestBody: PostUserRequest = request.body;
+  const UserNumber: number = await client.user.count();
   await client.user.create({
-    data: { name: request.body.name },
+    data: { name: requestBody.name },
   });
-  response.send();
+  const responseBody: PostUserResponse = {
+    id: UserNumber + 1,
+    name: requestBody.name,
+  };
+  response.json(responseBody);
 });
 
 // Userを全て取得
+
 app.get("/user", async (_, response) => {
   const users = await client.user.findMany();
   response.json(users);
 });
 
 // 名前の変更
+
+type PutUserRequest = {
+  name: string;
+  id: number;
+};
+
 app.put("/user", async (request, response) => {
+  const requestBody: PutUserRequest = request.body;
   await client.user.update({
-    where: { id: request.body.id },
-    data: { name: request.body.name },
+    where: { id: requestBody.id },
+    data: { name: requestBody.name },
   });
   response.send();
 });
 
+// Get user by ID
+
+type GetUserIdRequest = {
+  userId: number;
+};
+
+app.get("/user/userId", async (request, response) => {
+  const requestBody: GetUserIdRequest = request.body;
+  const user = await client.user.findUnique({
+    where: { id: requestBody.userId },
+  });
+  response.json(user);
+});
+
 // プログラムの挿入とUserBattleIdentityの作成
+
+type PutProgramRequest = {
+  userId: number;
+  program: string;
+};
+
 app.put("/program", async (request, response) => {
+  const requestBody: PutProgramRequest = request.body;
+  const participantNumber = await client.userBattleIdentity.count();
   await client.userBattleIdentity.create({
     data: {
-      user: client.user,
-      program: request.body.program,
-      league: 1,
+      userId: requestBody.userId,
+      program: requestBody.program,
+      league: Math.floor((participantNumber + 5) / 4), // 作成された順にリーグ番号が割り振られる
     },
   });
   response.send();
